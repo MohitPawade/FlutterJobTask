a42e9aa31755498237b44e7a82f7a100a202d213
Mohitp1 <Mohit.Pawade@kpit.com>
2021-12-20 13:52:32 +0530

    Setup and Package Installation

---
+++ flutterjobtask/android/app/src/main/java/com/flutterjobtask/flutterjobtask/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutterjobtask.flutterjobtask;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutterjobtask/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterJobTask());
+}
+
+class FlutterJobTask extends StatelessWidget {
+  const FlutterJobTask({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Container(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {},
+        tooltip: 'Next',
+        child: const Icon(Icons.arrow_forward),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
